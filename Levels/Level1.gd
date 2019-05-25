41cd7bf09c036cd217a96b7a02fcde3e53be3d2b
PseudoC0de <pcodevoid@gmail.com>
2019-05-25 09:31:30 -0500

    Added walls and initial animation

--- Levels/Level1.gd
+++ Levels/Level1.gd
@@ -11,3 +11,8 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta: float) -> void:
 #	pass
+
+
+func _on_Exit_body_entered(body: PhysicsBody2D) -> void:
+	if body.name == "TC":
+		get_tree().change_scene("res://Levels/Entrance.tscn")
--- Player/TC.gd
+++ Player/TC.gd
@@ -1,22 +1,46 @@
 extends KinematicBody2D
 
-#var motion = Vector2()
-#const UP = Vector2(0,-1)
-var speed = 500
+var motion = Vector2()
+const FLOOR = Vector2(0,-1)
+const speed = 500
+const gravity = 25
+const jump = -900
+const MAX_GRAVITY = 50
+var jump_count = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
+	print(str(motion.x) + ", " + str(motion.y))
+	if motion.y < 50:
+		motion.y += gravity
+	
 	if Input.is_action_pressed("ui_right"):
-		move_local_x(speed * delta, true)
+		motion.x = speed
 		$Sprite.flip_h = true
 		$Sprite.play("walking")
 	elif Input.is_action_pressed("ui_left"):
-		move_local_x(-speed * delta)
+		motion.x = -speed
 		$Sprite.flip_h = false
 		$Sprite.play("walking")
 	else:
+		motion.x = 0
 		$Sprite.play("idle")
+	
+	if jump_count < 2:
+		if Input.is_action_just_pressed("ui_up"):
+			motion.y = jump
+			jump_count += 1
+		
+	if is_on_floor():
+		jump_count = 0
+		if Input.is_action_just_pressed("ui_up"):
+			motion.y = jump
+			jump_count += 1
+	
+	motion.y += gravity
+	
+	motion = move_and_slide(motion, FLOOR)
