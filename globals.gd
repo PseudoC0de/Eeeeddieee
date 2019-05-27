7280601b3c2e87c57bae88db2771a130079a4db4
PseudoC0de <pcodevoid@gmail.com>
2019-05-27 10:05:39 -0500

    minor adjustments

--- globals.gd
+++ globals.gd
@@ -7,13 +7,13 @@ var intro_finished = false
 var boss_intro_finished = false
 var boss_head_health = BOSS_HEAD_HEALTH
 var boss_middle_health = BOSS_MIDDLE_HEALTH
-const BOSS_HEAD_HEALTH = 25
-const BOSS_MIDDLE_HEALTH = 10
+const BOSS_HEAD_HEALTH = 50
+const BOSS_MIDDLE_HEALTH = 25
 var phase = 0
 var won = false
 var TC_position
 var enemies_killed = 0
-var bat1_health = 15
+var bat1_health = 30
 var bat2_health = 15
 
 #func _input(event):
